23eccdaaeacc4fb4c107338f2decd4429bfc33a7
Sergey Belov <belovtorzhok@gmail.com>
2013-07-15 11:03:40 +0400

    Update gem name

--- lib/deployconfig.rb
+++ lib/deployconfig.rb
@@ -6,7 +6,5 @@ require "whenever/capistrano"
 require "delayed/recipes"
 require "capistrano/ext/multistage"
 require "dotenv/capistrano"
-require "dotenv/capistrano"
-
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
 
