15f7dd9e91a83a1258758ac08decf695edc44337
Sergey Belov <belovtorzhok@gmail.com>
2013-07-15 14:46:44 +0400

    It works local!

--- lib/deployconfig.rb
+++ lib/deployconfig.rb
@@ -1,2 +1,10 @@
+  require 'capistrano'
+  require 'capistrano/cli'
+  require "capistrano-rbenv"
+  require "bundler/capistrano"
+  require "whenever/capistrano"
+  require "delayed/recipes"
+  require "capistrano/ext/multistage"
+  require "dotenv/capistrano"
 Dir.glob(File.join(File.dirname(__FILE__), '/recipes/*.rb')).sort.each { |f| load f }
 
