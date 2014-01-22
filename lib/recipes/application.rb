cc963479e0ba5b77dcc0a00909da723220d2c0a7
Sergey Belov <sergey.belov@saltpepper.ru>
2014-01-22 17:02:09 +0400

    Fix stages

--- lib/recipes/application.rb
+++ lib/recipes/application.rb
@@ -1,6 +1,7 @@
+# encoding: utf-8
 Capistrano::Configuration.instance.load do
 
-  set :git_application_name,             defer{ application }
+  set :git_application_name, defer{ application }
   set :deploy_to_application_name , defer{ application }
 
   set :deploy_env, defer { rails_env }
@@ -13,7 +14,7 @@ Capistrano::Configuration.instance.load do
   # Capistrano config
   set :scm, :git
   set :repository, defer { "ssh://git@git.snpdev.ru:42204/#{application}.git" }
-  #set :branch, "master"
+  # set :branch, "master"
   set :git_shallow_clone, 1 # only master branch
   set :deploy_to, defer { "/var/www/#{deploy_to_application_name}" }
 
@@ -24,13 +25,13 @@ Capistrano::Configuration.instance.load do
   set :normalize_asset_timestamps, false
 
   # Bundle config
-  set :bundle_binary, "bundle"
-  set :bundle_flags,  "--deployment --binstubs"
-  set :bundle_without, defer { [:development, :test, :production] - [ rails_env ] }
+  set :bundle_binary, 'bundle'
+  set :bundle_flags,  '--deployment --binstubs'
+  set :bundle_without, defer { [:development, :test, :production] - [rails_env] }
 
   # Unicorn config
-  set :unicorn_binary, "unicorn"
-  #set :unicorn_config, defer { "#{current_path}/config/unicorn/#{stage}.rb" }
+  set :unicorn_binary, 'unicorn'
+  # set :unicorn_config, defer { "#{current_path}/config/unicorn/#{stage}.rb" }
   set :unicorn_config, defer { "#{current_path}/config/unicorn.rb" }
   set :unicorn_pid, defer { "#{deploy_to}/shared/pids/unicorn.pid" }
 
@@ -39,14 +40,16 @@ Capistrano::Configuration.instance.load do
   set :whenever_environment, defer { rails_env }
   set :whenever_identifier, defer { "#{application}_#{stage}" }
 
-  set :application,                             defer{ application }
+  set :application, defer { application }
   # rbenv
-  set :rbenv_ruby_version, "1.9.3-p392"
+  set :rbenv_ruby_version, '1.9.3-p392'
 
-  if fetch(:stages).nil?
+  begin
+    fetch(:stages)
+  rescue IndexError
     set :stages, %w(testing production)
   end
-  set :default_stage, "testing"
+  set :default_stage, 'testing'
   require 'capistrano/ext/multistage'
 
 end
