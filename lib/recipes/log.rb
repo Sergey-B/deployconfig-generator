07ea895bf846af03741a72de705bd51fbe3a356d
Sergey Belov <sergey.belov@saltpepper.ru>
2014-04-14 17:17:06 +0400

    Changed log:tail recipe, allowed choose log on remote server to tail

--- lib/recipes/log.rb
+++ lib/recipes/log.rb
@@ -2,7 +2,8 @@ Capistrano::Configuration.instance.load do
   namespace :log do
     desc "Tail all application log files"
     task :tail, :roles => :app do
-      run "tail -f -n 100 #{shared_path}/log/*.log" do |channel, stream, data|
+      log_name = ENV.fetch('LOG_NAME') { '*' }
+      run "tail -f -n 100 #{shared_path}/log/#{log_name}.log" do |channel, stream, data|
         puts "#{channel[:host]}: #{data}"
         break if stream == :err
       end
