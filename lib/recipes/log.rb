4a652dabab6d0ac993d619784b74eb8a3281b345
Sergey Belov <sergey.belov@saltpepper.ru>
2014-04-23 12:22:07 +0400

    Changed log tail recipe

--- lib/recipes/log.rb
+++ lib/recipes/log.rb
@@ -2,11 +2,19 @@ Capistrano::Configuration.instance.load do
   namespace :log do
     desc "Tail all application log files"
     task :tail, :roles => :app do
-      log_name = ENV.fetch('LOG_NAME') { '*' }
-      log_list = `ls ./log/ | awk '{ print $0 }'`.split
-      run "tail -f -n 100 #{shared_path}/log/\"#{log_name}\".log" do |channel, stream, data|
-        puts "#{channel[:host]}: #{data}"
-        break if stream == :err
+      log_name = ENV.fetch('LOG_NAME') { "*" }
+      run "ls  #{shared_path}/log/" do |channel, stream, data|
+        remote_logs = data.split
+        HighLine.new.choose do |menu|
+          menu.prompt = 'Please select log: '
+          menu.choices(*remote_logs) do |choice|
+            choice = choice.sub(/.log$/,'\\1')
+            run "tail -f -n 100 #{shared_path}/log/#{choice}.log" do |channel, stream, data|
+              puts "#{channel[:host]}: #{data}"
+              break if stream == :err
+            end
+          end
+        end
       end
     end
   end
